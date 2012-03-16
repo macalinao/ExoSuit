3ad38236f1c4c8a247d72573028494c56533577a
Ian Macali <ianmacalinao@gmail.com>
2012-03-16 01:04:29 -0500

    Remove the player from exosuit when they logout.

--- src/main/java/net/celestiusmc/exosuit/ExoSuitListener.java
+++ src/main/java/net/celestiusmc/exosuit/ExoSuitListener.java
@@ -12,7 +12,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.util.Vector;
 
 /**
@@ -42,6 +44,16 @@ public class ExoSuitListener implements Listener {
                 jumping.add(player);
             }
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        jumping.remove(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerKick(PlayerKickEvent event) {
+        jumping.remove(event.getPlayer());
     }
 
     @EventHandler
